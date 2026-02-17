8d8a3b05231b2be2ed5968beafa77b567630e516
Rihza07 <rihzagaming123@gmail.com>
2026-02-17 15:33:33 +0800

    Initial commit of Next.js application

---
+++ app/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 I-Smart. All rights reserved.</p>
+    </footer>
+  )
+}
---
+++ app/components/Header.jsx
@@ -0,0 +1,56 @@
+
+
+export default function Header() {
+  return (
+    <nav>
+        <div className="container nav-container">
+            <h1 className="logo">OS-Cendekia</h1>
+
+            <div className="hamburger" id="hamburgerBtn">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            <ul className="nav-links" id="navLinks">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#Guest">Guest Star</a></li>
+                <li><a href="#competition">Competition</a></li>
+                <li><a href="#timeline">Timeline</a></li>
+                <li id="navRiwayatWrapper" className="hidden"><a href="#userDashboard">Riwayat</a></li>
+                <li><a href="#faq">FAQ</a></li>
+                <li><a href="#contact">Contact</a></li>
+
+
+                <li className="mobile-login-wrapper">
+                    <button id="loginBtnMobile" className="btn login-btn mobile-login-btn">Login with Google</button>
+                </li>
+
+
+                <li className="mobile-profile-wrapper">
+                    <div id="userProfileMobile" className="user-profile hidden">
+                        <div className="user-info">
+                            <span id="userNameMobile" className="u-name">User</span>
+                            <span id="userEmailMobile" className="u-email">email@mail.com</span>
+                        </div>
+                        <button id="logoutBtnMobile" className="logout-btn">Logout</button>
+                    </div>
+                </li>
+            </ul>
+
+            <div className="nav-actions">
+                <button id="loginBtn" className="btn login-btn">Login with Google</button>
+
+
+                <div id="userProfileDesktop" className="user-profile desktop-profile hidden">
+                    <div className="user-info">
+                        <span id="userNameDesktop" className="u-name">User</span>
+                        <span id="userEmailDesktop" className="u-email">email@mail.com</span>
+                    </div>
+                    <button id="logoutBtnDesktop" className="logout-btn">Logout</button>
+                </div>
+            </div>
+        </div>
+    </nav> 
+  )
+}
